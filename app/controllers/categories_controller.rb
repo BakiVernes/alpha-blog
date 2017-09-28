5f391079670a368adc731276aa2d66e4f2e2a9c9
Baki Udemy Course <vernes.pendic@gmail.com>
2017-09-28 18:14:54 +0000

    Integration test

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,28 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+  
+  def new
+    @category = Category.new
+  end
+  
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Catogry was successfully created"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+  
+  def show
+    
+  end
+end
